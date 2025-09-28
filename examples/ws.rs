c139036bafb36a24fc0d03245980b858b7a98ae8
leo <leo233.huang@gmail.com>
2025-09-28 10:10:43 +0800

    fix websocket example

--- examples/ws.rs
+++ examples/ws.rs
@@ -1,44 +1,25 @@
-use axum::{
-    body::Bytes,
-    extract::{
-        ws::{Message, WebSocket, WebSocketUpgrade},
-        Path,
-    },
-    response::IntoResponse,
-    routing::any,
-    Extension, Router,
-};
+use futures_util::{SinkExt, StreamExt};
+use log::{debug, info};
+use tokio::net::TcpListener;
+use tokio_websockets::{Message, ServerBuilder};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    // 加载config.toml
-    //ws://10.2.12.169:8765
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8765").await.unwrap();
 
+    let listener = TcpListener::bind("0.0.0.0:8765").await?;
+    info!("Listening on 0.0.0.0:8765");
     loop {
-        let (socket, _) = listener.accept().await?;
-        let mut std_tcp_stream = socket.into_std()?;
+        let (conn, _) = listener.accept().await?;
+        let (_request, mut ws_stream) = ServerBuilder::new().accept(conn).await?;
+        ws_stream.send(Message::text("Hello, client!")).await?;
+        while let Some(Ok(item)) = ws_stream.next().await {
+            if item.is_binary() || item.is_text() {
+                debug!("Received: {:?}", item);
+                ws_stream.send(item).await?;
+            }
+        }
     }
 
-    // if let Err(e) = axum::serve(listener, routes().await).await {
-    //     log::error!("Server error: {}", e);
-    // } else {
-    //     log::warn!("Server exit");
-    // }
+    // Ok(())
 }
-
-// async fn routes() -> Router {
-//     log::info!("Start ");
-//     println!("hello");
-//     let mut router = Router::new().route("/ws/{id}", any(ws_handler));
-
-//     router
-// }
-
-// async fn ws_handler() -> impl IntoResponse {
-//     let (tx, rx) = tokio::sync::mpsc::unbounded_channel::<u8>();
-//     log::info!("ws_handler tx: {:?}", tx);
-//     println!("world");
-//     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-// }
